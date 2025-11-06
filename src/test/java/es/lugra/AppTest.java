6264e1dfa47df3e17a0218f7e4c3f3edb9207b68
Lugra <lugra.dev@gmail.com>
2025-11-06 19:15:01 +0100

    first commit

---
+++ src/test/java/es/lugra/AppTest.java
@@ -0,0 +1,20 @@
+package es.lugra;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
